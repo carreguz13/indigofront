9177713ff16b105284c4efa1a46bcfb240a77999
carreguz13 <carreguz13@gmail.com>
2023-05-09 16:29:11 -0700

    home page terminada sin estilos

--- src/App.jsx
+++ src/App.jsx
@@ -9,30 +9,47 @@ function App() {
 
   return (
     <div className="main">
-      <table>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Nombre</th>
-            <th>Edad</th>
-            <th>Grupo</th>
-          </tr>
-        </thead>
-        <tbody>
-          {array.map((lol) => (
+      <div>
+        <div className="search-bar">
+          <input type="search" />
+          <button>registrar nuevo alumno</button>
+        </div>
+        <select name="select">
+          <option value="value1" selected>
+            Value 1
+          </option>
+          <option value="value2">Value 2</option>
+          <option value="value3">Value 3</option>
+        </select>
+        <table>
+          <thead>
             <tr>
-              <td>{lol.id}</td>
-              <td>{lol.nombre}</td>
-              <td>{lol.edad}</td>
-              <td>{lol.grupo}</td>
-              <td>
-                <button>editar</button>
-                <button>eliminar</button>
-              </td>
+              <th>Id</th>
+              <th>Nombre</th>
+              <th>Edad</th>
+              <th>Grupo</th>
+              <th>Pago</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {array.map((lol) => (
+              <tr>
+                <td>{lol.id}</td>
+                <td>{lol.nombre}</td>
+                <td>{lol.edad}</td>
+                <td>{lol.grupo}</td>
+                <td>
+                  <button>Enviar link de pago</button>
+                </td>
+                <td>
+                  <button>editar</button>
+                  <button>eliminar</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
