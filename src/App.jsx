79e74c47b3c0d502ea97f8e0a95b8ac36f7eecd0
carreguz13 <carreguz13@gmail.com>
2023-05-09 15:36:39 -0700

    se agregaron botones de editar y eliminar a la tabla

--- src/App.jsx
+++ src/App.jsx
@@ -10,21 +10,28 @@ function App() {
   return (
     <div className="main">
       <table>
-        <tr>
-          <th>Id</th>
-          <th>Nombre</th>
-          <th>Edad</th>
-          <th>Grupo</th>
-        </tr>
-
-        {array.map((lol) => (
+        <thead>
           <tr>
-            <td>{lol.id}</td>
-            <td>{lol.nombre}</td>
-            <td>{lol.edad}</td>
-            <td>{lol.grupo}</td>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Edad</th>
+            <th>Grupo</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {array.map((lol) => (
+            <tr>
+              <td>{lol.id}</td>
+              <td>{lol.nombre}</td>
+              <td>{lol.edad}</td>
+              <td>{lol.grupo}</td>
+              <td>
+                <button>editar</button>
+                <button>eliminar</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
