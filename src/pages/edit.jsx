90deff201f294ca9a1dc1636b90b35dfb331ad93
carreguz13 <carreguz13@gmail.com>
2023-06-23 10:15:24 -0700

    test change of branch to main

--- src/pages/edit.jsx
+++ src/pages/edit.jsx
@@ -23,6 +23,8 @@ function Edit() {
   const location = useLocation();
   const alumnoid = location.pathname.split("/")[2];
 
+  console.log(1);
+
   async function editar(e) {
     e.preventDefault();
     try {
